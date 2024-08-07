06789a28022b2a8f9f1a40ee672cd95b8ac34c1f
chotixity <melkychoti@gmail.com>
2024-08-07 14:52:11 +0300

    changes to gitignore

--- lib/provider/auth_provider.dart
+++ lib/provider/auth_provider.dart
@@ -1,10 +1,12 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:podcasts/services/auth.dart';
 
 enum AuthenticationStatus { unknown, authenticated, unauthenticated }
 
 class AuthProvider with ChangeNotifier {
+  final Auth _auth = Auth();
   final _controller = StreamController<AuthenticationStatus>();
 
   Stream<AuthenticationStatus> get status async* {
@@ -12,5 +14,8 @@ class AuthProvider with ChangeNotifier {
     yield* _controller.stream;
   }
 
-  void logIn() {}
+  void SignUp(String email, String password) {
+    _auth.signUpWithEmailAndPassword(email, password);
+    _controller.add(AuthenticationStatus.authenticated);
+  }
 }
