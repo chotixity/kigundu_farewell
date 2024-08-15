6e0282742fd61124851ade8346a8dc192e636101
chotixity <melkychoti@gmail.com>
2024-08-15 16:10:41 +0300

    :sparkles: added ability to share podcast

--- lib/features/share/share.dart
+++ lib/features/share/share.dart
@@ -1,10 +1,25 @@
-import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+import 'package:share_plus/share_plus.dart';
+import 'package:path_provider/path_provider.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+Future<String> downloadAndRenameAudio(String url) async {
+  final dio = Dio();
+  final tempDir = await getTemporaryDirectory();
+  final tempPath = tempDir.path;
 
-  @override
-  Widget build(BuildContext context) {
-    return const Icon(Icons.share);
-  }
+  // Extract file name from URL, without query parameters
+  final uri = Uri.parse(url);
+  final originalFileName =
+      uri.pathSegments.last.split('?').first; // Remove query parameters
+  final filePath = '$tempPath/$originalFileName';
+
+  // Download the file to temporary location
+  await dio.download(url, filePath);
+
+  // Return the new file path
+  return filePath;
+}
+
+void shareAudio(String filePath) {
+  Share.shareXFiles([XFile(filePath)], text: 'Check out this audio!');
 }
